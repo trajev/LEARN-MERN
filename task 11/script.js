43732b3aab4c471261005dadf93a2ba55d18372d
T Rajeev <telagasettirajeev999@gmail.com>
2024-05-30 20:40:19 +0530

    task 11 mern course

---
+++ task 11/script.js
@@ -0,0 +1,28 @@
+let a = 10;
+let b = 5;
+let c = 7;
+let d = 62;
+
+if ( a%2===0 ){
+    console.log("The number " + a + " is even." );
+} else {
+    console.log("The number " + a + " is odd.");
+}
+
+if ( b%2===0 ){
+    console.log("The number " + b + " is even." );
+} else {
+    console.log("The number " + b + " is odd.");
+}
+
+if ( c%2===0 ){
+    console.log("The number " + c + " is even." );
+} else {
+    console.log("The number " + c + " is odd.");
+}
+
+if ( d%2===0 ){
+    console.log("The number " + d + " is even." );
+} else {
+    console.log("The number " + d + " is odd.");
+}
